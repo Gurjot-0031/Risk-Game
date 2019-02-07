b8bee29a9820749aefe0f73887fc010a142e8569
Gurjot Singh <gurjot5076@gmail.com>
2019-02-07 16:44:52 -0500

    commit

---
+++ src/com/riskgame/main.java
@@ -0,0 +1,7 @@
+package com.riskgame;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Gurjot Singh");
+    }
+}
--- src/test_jai.java
+++
@@ -1,8 +0,0 @@
-public class test_java{
-    public static void main(String[] args) {
-        String a;
-        a = "JAI_TESTING";
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
