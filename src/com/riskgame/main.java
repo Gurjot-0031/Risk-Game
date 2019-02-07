b5a8e8743baecfbcac4da2a5fb0bb50aa04b48a1
Ashmeet Singh <ashu6811singh@gmail.com>
2019-02-07 16:59:04 -0500

    all set

--- src/com/riskgame/main.java
+++ src/com/riskgame/main.java
@@ -2,6 +2,6 @@ package com.riskgame;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Gurjot Singh");
+        System.out.println("Gurjot Singh"); System.out.println("Gurjot Singh");
     }
 }
