b268d908f151fc7bd044a35abc7de7fdf56e8f13
Ashmeet Singh <ashu6811singh@gmail.com>
2019-02-09 14:14:45 -0500

    main update done

--- src/com/riskgame/RiskGame/Main.java
+++ src/com/riskgame/RiskGame/Main.java
@@ -4,9 +4,20 @@ import com.riskgame.Controllers.HomeController;
 
 import javax.swing.*;
 
+/**
+ * Main class to run the Risk Game.
+ *
+ * @author Ashmeet
+ * @version 1.2
+ */
+
 public class Main {
+    /**
+     * To start the intialize and start the risk game using the main function.
+     * @param args Arguments used for the Main class.
+     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
