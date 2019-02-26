91503d789a881b5b9bcc2d82c5f625ec6c30874f
Ashmeet <ashmeet.singh6811@gmail.com>
2019-02-26 15:36:21 -0500

    Map Editor and Events

--- src/Controller/MapEditorController.java
+++ src/Controller/MapEditorController.java
@@ -1,5 +1,16 @@
 package Controller;
 
-public class MapEditorController {
+import Event.IEvent;
+import View.MapEditorView;
 
+public class MapEditorController {
+	public void eventTriggered(IEvent event) {
+		String eventInfo = event.getEventInfo();
+		System.out.println(eventInfo + "triggered at Map Editor Controller");
+		
+		switch(eventInfo) {
+			case "MapEditorBrowse":
+				break;
+		}
+	}
 }
--- src/View/MapEditorView.java
+++ src/View/MapEditorView.java
@@ -1,13 +1,18 @@
 package View;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import Controller.HomeController;
 import Controller.MapEditorController;
+import Event.MapEditorEvents;
 
 public class MapEditorView {
 	private static MapEditorController objController;
@@ -47,6 +52,27 @@ public class MapEditorView {
 		JButton btnBrowse = new JButton("Browse");
 		mapEditorFrame.getContentPane().add(btnBrowse);
 		btnBrowse.setBounds(120, 10, 100, 20);
+		btnBrowse.addMouseListener(new MouseListener() {
+			@Override public void mouseReleased(MouseEvent e) {}
+			@Override public void mousePressed(MouseEvent e) {}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				int returnValue = fileChooser.showOpenDialog(null);
+			    if (returnValue == JFileChooser.APPROVE_OPTION) 
+			    {
+				    File selectedFile = fileChooser.getSelectedFile();
+				    System.out.println("File Selected: " + selectedFile.getAbsolutePath());
+				    MapEditorEvents objEvent = new MapEditorEvents();
+				    objEvent.setEventFile(selectedFile);
+				    objEvent.setEventInfo("MapEditorBrowse");
+				    objController.eventTriggered(objEvent);
+			    }
+				return;
+			}
+			@Override public void mouseEntered(MouseEvent e) {}
+			@Override public void mouseExited(MouseEvent e) {}
+		});
 	}
 	
 	
